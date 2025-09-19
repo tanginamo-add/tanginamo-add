efd1d0c660944d4ddacbb4e28298f52d3b6e091a
tanginamo-add <neilandrewmagnate@gmail.com>
2025-09-19 21:57:10 +0800

    Add files via upload

---
+++ ade.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string answer;
+    cout << "miss mo ba ako:";
+    cin >> answer;
+
+    if (answer == "oo" || answer == "yes" || answer == "y" || answer == "yess" || answer == "yesss" || answer == "yessss" || answer == "yesssss" || answer == "yesssssss" || answer == "yessssssss") {
+        cout<<"missyoutoo:)";
+    }
+    
+    else if (answer == "hindi" || answer == "no" || answer == "n" || answer == "noo" || answer == "nooo" || answer == "noooo" || answer == "nooooo" || answer == "noooooo" || answer == "nooooooo") {
+        cout<<"awts gege:(";
+    }
+   {
+       cout<<"awts gege:(";
+   }
+   
+
+    return 0;
+}
